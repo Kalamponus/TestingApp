68b7701519a4e293ea35ab072e6e25476fc9da2b
Kalamponus <m.maiorov@inbox.ru>
2023-10-04 15:26:43 +0300

    Добавьте файлы проекта.

---
+++ Views/Windows/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace TestingApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
